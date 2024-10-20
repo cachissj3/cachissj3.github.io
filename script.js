4f8f5de8e2a072fc9188044e630bab02df8711a3
Ezequiel <130161620+cachissj3@users.noreply.github.com>
2024-10-20 19:11:58 -0300

    nueva funcion, a los anchor y creo que lista la primera pagina(nose que mas agregar)

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 const imagenes = [
-    'img/carrusel1.jpeg',
-    'img/carrusel2.jpeg',
-    //falta agregar una imagen, con un tamaño similar para que no quede mal
-  ];
-  
-  let contador = 0;
-  const imgElement = document.getElementById('iCarrusel');
-  
-  function adelante() {
-    contador++;
-    if (contador > imagenes.length - 1) {
-      contador = 0;
-    }
-    imgElement.src = imagenes[contador];
+  'img/carrusel1.jpeg',
+  'img/carrusel2.jpeg',
+  //falta agregar una imagen, con un tamaño similar para que no quede mal
+];
+
+let contador = 0;
+const imgElement = document.getElementById('iCarrusel');
+
+function adelante() {
+  contador++;
+  if (contador > imagenes.length - 1) {
+    contador = 0;
   }
-  
-  function atras() {
-    contador--;
-    if (contador < 0) {
-      contador = imagenes.length - 1;
-    }
-    imgElement.src = imagenes[contador];
+  imgElement.src = imagenes[contador];
+}
+
+function atras() {
+  contador--;
+  if (contador < 0) {
+    contador = imagenes.length - 1;
   }
-  
-  document.getElementById('bAdelante').addEventListener('click', adelante);
-  document.getElementById('bAtras').addEventListener('click', atras);
-  
-  function iniciarCarrusel() {
-    imgElement.src = imagenes[contador];
-    setInterval(MoverDerecha, 5000);
-  }
-  
-  iniciarCarrusel();
+  imgElement.src = imagenes[contador];
+}
+
+document.getElementById('bAdelante').addEventListener('click', adelante);
+document.getElementById('bAtras').addEventListener('click', atras);
+
+function iniciarCarrusel() {
+  imgElement.src = imagenes[contador];
+  setInterval(adelante, 5000);
+}
+function abrirCaja(e) {
+  var id = e.currentTarget.getAttribute('href').split('#')[1];
+  const infos = document.querySelectorAll('.info');
+  infos.forEach(info => info.style.display = 'none');
+  document.getElementById(id).style.display = 'block';
+}
+iniciarCarrusel();
